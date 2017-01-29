ad133bb4e02eb82b5a09310cca936a4bd6989361
Ismael <ismael.felipa@gmail.com>
2017-01-29 20:16:51 +0100

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,4 +1,11 @@
-if (window.top !== window.self){alert("iframe");}
+if (window.top == window.self){
+    alert("iframe");
+    var elements = document.querySelectorAll('link[rel=stylesheet]');
+    for(var i=0;i<elements.length;i++){       
+       console.log(elements[i].parentNode.removeChild(elements[i]);)
+    }
+
+}
 var send = [];
 send[0] = 'hello';
 document.addEventListener("mouseup", function(){
