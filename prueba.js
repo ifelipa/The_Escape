152a52721a4b22cdd1c4b0fc9a0215fc462b0804
Ismael <ismael.felipa@gmail.com>
2017-01-29 14:18:15 +0100

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,1 +1,10 @@
 window.top.postMessage('hello', '*')
+var txt='';
+if (window.getSelection) {
+		        txt = window.getSelection();
+		    } else if (window.document.getSelection) {
+		        txt =window.document.getSelection();
+		    } else if (window.document.selection) {
+		        txt = window.document.selection.createRange().text;
+		    }
+console.log(txt)
