bfdf269b76902e158ac986a01445b62f8213c8c1
Ismael <ismael.felipa@gmail.com>
2017-01-29 18:30:42 +0100

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -3,7 +3,7 @@ send[0] = 'hello';
 window.top.postMessage(send, '*')
  document.addEventListener("mouseup", function(){
  	if(document.getSelection()){
-         	var send[1] = document.getSelection();
+         	send[1] = document.getSelection();
          	console.log("sele "+send[1]);        
     		}
   });
