d255f9160c48ce23f4f05132dbc69dfb256ed394
Ismael <ismael.felipa@gmail.com>
2017-01-29 17:37:12 +0100

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,9 +1,12 @@
-window.top.postMessage('hello', '*')
- var txt='';
+ var send=[];
  document.addEventListener("mouseup", function(){
  	if(document.getSelection()){ //Resto de navegadores
-         	var sel = document.getSelection();
-         	console.log("selec2"+sel);
+         	send[1] = document.getSelection();
+         	console.log("selec2"+ send[1]);
+          send[0]=true;
     		}
 
   });
+if (send[0]){
+ window.top.postMessage('hello', '*')
+}
